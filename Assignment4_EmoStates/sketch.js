0f274566f104c7904ea2dd42d50bb3cbbc5df985
julianacevedo <31459624+julianacevedo@users.noreply.github.com>
2017-10-04 19:46:12 -0400

    Draft 2

--- Assignment4_EmoStates/sketch.js
+++ Assignment4_EmoStates/sketch.js
@@ -7,16 +7,17 @@ var circY;
 var circDiameter = 60;
 var circRadius = circDiameter/2;
 var d;
-var isClicked =false;
+//var isClicked =false;
 var col1 = 30;
 var col2 = 200;
+var state = 1;
+var words =  text("Ooh lala",width/3,570);
 
 function preload(){
     img1 = loadImage("MrBean1.jpg");
     img2 = loadImage("MrBean2.jpg");
     img3 = loadImage("MrBean3.jpg");
     neutraFont = loadFont("NeutraDisp-Bold.otf");
-    
 }
 
 function setup() {
@@ -32,54 +33,32 @@ function draw() {
     imageMode(CENTER);
     image (img1,width/2, height/2, 500, 500);
     fill(40,10,215);
-    text("Oooh lala",width/3,570);
+    words = text("Ooh lala",width/3,570);
     ellipse(circX,circY,circDiameter,circDiameter);
     d =dist(circX, circY, mouseX, mouseY);  
-    
-        if (d < circRadius && isClicked ==false){
-        imageMode(CENTER);
-        image (img2,width/2, height/2, 500, 500);
-        col1=30;
-        col2=200;
-        fill(0);
-    ellipse(circX,circY,circDiameter,circDiameter);
-    d =dist(circX, circY, mouseX, mouseY);  
-
-        fill(40,10,215);
-        text("What is that?",width/3,570);
-        isClicked = !isClicked;
-    }
-    else if (d < circRadius && isClicked ==true){
-        imageMode(CENTER);
-        image (img3,width/2, height/2, 500, 500);
-        col1=220;
-        col2=30;
-        fill(240,00,215);
-        text("very funny!",width/3,570);
-        isClicked = !isClicked;
-        ellipse(circX,circY,circDiameter,circDiameter);
-    d =dist(circX, circY, mouseX, mouseY);
-    }
     
 }
 
 function mousePressed(){
-    if (d < circRadius && isClicked == true){
-//        imageMode(CENTER);
-//        image (img1,width/2, height/2, 500, 500);
-//        col1=30;
-//        col2=200;
-//        fill(40,10,215);
-//        text("Very funny",width/3,570);
-        isClicked = !isClicked;
-    }else if (d < circRadius && isClicked ==false){
-//        imageMode(CENTER);
-//        image (img2,width/2, height/2, 500, 500);
-//        col1=220;
-//        col2=30;
-//        fill(240,00,215);
-//        text("funny still?",width/3,570);
-        isClicked = !isClicked;
+   if(d < circRadius && state == 1){
+        img1 = img2;
+        col1 = 30;
+        col2 = 200;
+        state = state +1;
+    }else if (d < circRadius && state == 2){
+        fill(40,10,115);
+        words = text("Ooooh lalala",width/3,570);
+        img2 = img3;
+        col1 = 100;
+        col2 = 30;
+        state = state+1;
+    }else if (d < circRadius && state == 3){
+        fill(40,10,115);
+        words = text("Ooooh lalalala",width/3,570);
+        img3 = img1;
+        col1 = 255;
+        col2 = 60;
+        state = 1;
     }
     
 }
